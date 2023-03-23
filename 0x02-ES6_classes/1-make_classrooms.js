50a700d7251989c86c73ca4c96af2edf386e37fa
abel mesfin <abelmesfin555@gmail.com>
2023-03-23 22:13:55 +0300

    inital commit

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,11 @@
-import ClassRoom from "./0-classroom.js";
-function initializeRooms() => {
+import ClassRoom from "./0-classroom";
+
+function initializeRooms() {
   return [
       new Classroom(19),
       new Classroom(20),
       new Classroom(34),
   ];
 }
+
 export default initializeRooms;
